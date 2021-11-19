80aee79a195d3ae3b1ccb3f21460663b58dbfd53
makkii-bcr <makkii@mkbcr.com>
2021-11-19 22:07:25 +0900

    インデント2→4space

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.static(__dirname + '/'));
 // HTTPS
 const server1 = https.createServer({
-  cert: readFileSync('key/cert1.pem'),
-  key: readFileSync('key/privkey1.pem')
+    cert: readFileSync('key/cert1.pem'),
+    key: readFileSync('key/privkey1.pem')
 }, app);
 const wss1 = new WebSocketServer({ server: server1 });
 // HTTP
@@ -24,86 +24,86 @@ setupServer(server1, wss1, 443);
 setupServer(server2, wss2, 80);
 
 function setupServer(server, wss, port) {
-  wss.on("connection", (ws, req) => {
-    const ip = req.socket.remoteAddress;
-    let playerNo = playerAry.length;
-    for (i in playerAry) {
-      const idx = parseInt(i);
-      if (playerAry[idx] == null) {
-        playerNo = idx;
-        break;
-      }
-    }
-
-    playerAry[playerNo] = { ws: ws, pingTime: performance.now() };
-    // console.log(playerAry.length);
-
-    playerCnt++;
-    console.log("connection", "playerCnt", playerCnt, "playerNo", playerNo, "ip", ip);
-    ws.send(JSON.stringify({ mes: "_start", playerCnt: playerCnt, playerNo: playerNo }));
-
-    ws.on('message', message => {
-      try {
-        // const t1 = performance.now();
-        const mesStr = message.toString();
-        let obj;
-        try {
-          obj = JSON.parse(mesStr);
-        } catch (e) { }
-        if (obj != null && obj.mes == "_ping") {
-          const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
-          if (pos != -1) playerAry[pos].pingTime = performance.now();
-        } else {
-          playerAry.forEach(client => {
-            if (client != null) {
-              client.ws.send(mesStr);
+    wss.on("connection", (ws, req) => {
+        const ip = req.socket.remoteAddress;
+        let playerNo = playerAry.length;
+        for (i in playerAry) {
+            const idx = parseInt(i);
+            if (playerAry[idx] == null) {
+                playerNo = idx;
+                break;
             }
-          });
         }
-        // const t2 = performance.now();
-        // console.log("message send time:", t2 - t1);
-      } catch (e) {
-        console.log("message send error:", e);
-      }
-    });
 
-    // 接続が切れた場合
-    ws.on('close', ws2 => {
-      const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
-      console.log("close", "playerCnt", pos);
-      if (pos != -1) {
-        playerAry[pos] = null;
-        if (playerCnt > 0) playerCnt--;
-        const json = JSON.stringify({ mes: "_close", playerNo: pos });
-        playerAry.forEach(client => {
-          if (client != null) client.ws.send(json);
+        playerAry[playerNo] = { ws: ws, pingTime: performance.now() };
+        // console.log(playerAry.length);
+
+        playerCnt++;
+        console.log("connection", "playerCnt", playerCnt, "playerNo", playerNo, "ip", ip);
+        ws.send(JSON.stringify({ mes: "_start", playerCnt: playerCnt, playerNo: playerNo }));
+
+        ws.on('message', message => {
+            try {
+                // const t1 = performance.now();
+                const mesStr = message.toString();
+                let obj;
+                try {
+                    obj = JSON.parse(mesStr);
+                } catch (e) { }
+                if (obj != null && obj.mes == "_ping") {
+                    const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
+                    if (pos != -1) playerAry[pos].pingTime = performance.now();
+                } else {
+                    playerAry.forEach(client => {
+                        if (client != null) {
+                            client.ws.send(mesStr);
+                        }
+                    });
+                }
+                // const t2 = performance.now();
+                // console.log("message send time:", t2 - t1);
+            } catch (e) {
+                console.log("message send error:", e);
+            }
         });
-      }
-    });
-  });
 
-  // const port = 443;
-  // const port = 58888;
-  server.listen(port);
-  console.log("http://localhost:" + port + "/html");
-
-  setInterval(() => {
-    const t1 = performance.now();
-    // console.log("setInterval:" + t1);
-    playerAry.forEach(client => {
-      if (client != null) {
-        if (t1 - client.pingTime >= 30000) {
-          const pos = playerAry.findIndex((client2) => client2 ? client2.ws == client.ws : false);
-          if (pos != -1) playerAry[pos] = null;
-          if (playerCnt > 0) playerCnt--;
-          console.log("timeout", "playerCnt", pos);
-        } else {
-          client.ws.send(JSON.stringify({ mes: "_ping" }));
-        }
-      }
+        // 接続が切れた場合
+        ws.on('close', ws2 => {
+            const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
+            console.log("close", "playerCnt", pos);
+            if (pos != -1) {
+                playerAry[pos] = null;
+                if (playerCnt > 0) playerCnt--;
+                const json = JSON.stringify({ mes: "_close", playerNo: pos });
+                playerAry.forEach(client => {
+                    if (client != null) client.ws.send(json);
+                });
+            }
+        });
     });
-    // for (i in playerAry) {
-    //   if (playerAry[i]!=null) console.log("playerAry", i, "value", playerAry[i] == null ? "null" : t1 - playerAry[i].pingTime);
-    // }
-  }, 5000);
+
+    // const port = 443;
+    // const port = 58888;
+    server.listen(port);
+    console.log("http://localhost:" + port + "/html");
+
+    setInterval(() => {
+        const t1 = performance.now();
+        // console.log("setInterval:" + t1);
+        playerAry.forEach(client => {
+            if (client != null) {
+                if (t1 - client.pingTime >= 30000) {
+                    const pos = playerAry.findIndex((client2) => client2 ? client2.ws == client.ws : false);
+                    if (pos != -1) playerAry[pos] = null;
+                    if (playerCnt > 0) playerCnt--;
+                    console.log("timeout", "playerCnt", pos);
+                } else {
+                    client.ws.send(JSON.stringify({ mes: "_ping" }));
+                }
+            }
+        });
+        // for (i in playerAry) {
+        //   if (playerAry[i]!=null) console.log("playerAry", i, "value", playerAry[i] == null ? "null" : t1 - playerAry[i].pingTime);
+        // }
+    }, 5000);
 }
