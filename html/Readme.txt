0c776d66d574691449b317b962c2db06a2d548b9
makkii-bcr <makkii@mkbcr.com>
2021-11-27 17:01:05 +0900

    いろいろ変更
    - サーバーからpingを送るのを廃止
    - 誰か（自分も含む）接続したら通知を送る
    - タイムアウトの処理修正
    - tonyuのサンプルファイルを削除

--- html/Readme.txt
+++
@@ -1,4 +0,0 @@
-このフォルダは、Webサーバにアップロードしないと正常に動作しない可能性があります。
-詳しくは
-https://www.tonyu.jp/tonyu2/runtime.html
-を御覧ください。
--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const WebSocketServer = require("ws").Server;
 const http = require('http');
 const https = require('https');
 const readFileSync = require('fs').readFileSync;
-const express = require('express');
 const performance = require('perf_hooks').performance;
 
-const app = express();
-app.use(express.static(__dirname + '/'));
+const timeOutTime = 30000;
+
 // HTTPS
 const serverS = https.createServer({
     cert: readFileSync('key/cert1.pem'),
     key: readFileSync('key/privkey1.pem')
-}, app);
+});
 const wssS = new WebSocketServer({ server: serverS });
 // HTTP
-const serverN = http.createServer(app);
-const wssN = new WebSocketServer({ server: serverN });
+// const serverN = http.createServer();
+// const wssN = new WebSocketServer({ server: serverN });
 
 const spaceAry = [];
 
 setupServer(serverS, wssS, 443);
-setupServer(serverN, wssN, 80);
+// setupServer(serverN, wssN, 80);
 
 function setupServer(server, wss, port) {
     wss.on("connection", (ws, req) => {
+        const clientFamily = req.socket.remoteFamily;
         const clientIp = req.socket.remoteAddress;
         const clientPort = req.socket.remotePort;
         const pathname = req.url;
@@ -40,40 +40,49 @@ function setupServer(server, wss, port) {
         const space = spaceAry[pathname];
         const playerAry = space.playerAry;
 
+        // 接続してきたクライアントを登録
         let playerNo = playerAry.findIndex((value) => value == null);
         playerNo = playerNo == -1 ? playerAry.length : playerNo;
-        playerAry[playerNo] = {
+        const playerObj = {
             ws: ws,
             pingTime: performance.now(),
             ip: clientIp,
             port: clientPort,
+            isClose: false
         };
+        playerAry[playerNo] = playerObj;
         // console.log(playerAry.length);
-
         space.playerCnt++;
-        console.log("connection", "playerCnt", space.playerCnt, "playerNo", playerNo, "ip", clientIp, "port", clientPort, "pathname", pathname);
-        ws.send(JSON.stringify({ mes: "_start", playerCnt: space.playerCnt, playerNo: playerNo }));
+        console.log(
+            "connection",
+            // "playerCnt", space.playerCnt,
+            "playerNo", playerNo,
+            // "clientFamily", clientFamily,
+            "ip", clientIp,
+            "port", clientPort,
+            "pathname", pathname
+        );
 
+        // 接続情報をクライアントに送る
+        ws.send(JSON.stringify({
+            mes: "_start",
+            playerCnt: space.playerCnt,
+            playerNo: playerNo,
+            serverTime: new Date().getTime()
+        }));
+        // 接続したことを全員に知らせる
+        sendEvery(playerAry, JSON.stringify({
+            mes: "_connection",
+            playerNo: playerNo,
+            serverTime: new Date().getTime()
+        }));
+
+        // メッセージ受信
         ws.on('message', message => {
             try {
-                // const t1 = performance.now();
-                const mesStr = message.toString();
-                let obj;
-                try {
-                    obj = JSON.parse(mesStr);
-                } catch (e) { }
-                if (obj != null && obj.mes == "_ping") {
-                    const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
-                    if (pos != -1) playerAry[pos].pingTime = performance.now();
-                } else {
-                    playerAry.forEach(client => {
-                        if (client != null) {
-                            client.ws.send(mesStr);
-                        }
-                    });
-                }
-                // const t2 = performance.now();
-                // console.log("message send time:", t2 - t1);
+                // メッセージが来たら全員にばらまく
+                playerObj.pingTime = performance.now();
+                sendEvery(playerAry, message.toString());
             } catch (e) {
                 console.log("message send error:", e);
             }
@@ -81,15 +90,24 @@ function setupServer(server, wss, port) {
 
         // 接続が切れた場合
         ws.on('close', ws2 => {
+            if (playerObj.isClose) return;
             const pos = playerAry.findIndex((client) => client ? client.ws == ws : false);
-            console.log("close", "playerCnt", pos);
+            console.log(
+                "close",
+                "playerNo", pos,
+                "ip", clientIp,
+                "port", clientPort,
+                "pathname", pathname
+            );
             if (pos != -1) {
                 playerAry[pos] = null;
                 if (space.playerCnt > 0) space.playerCnt--;
-                const json = JSON.stringify({ mes: "_close", playerNo: pos });
-                playerAry.forEach(client => {
-                    if (client != null) client.ws.send(json);
-                });
+                // 切断したことを全員に知らせる
+                sendEvery(playerAry, JSON.stringify({
+                    mes: "_close",
+                    playerNo: pos,
+                    serverTime: new Date().getTime()
+                }));
             }
         });
     });
@@ -99,38 +117,63 @@ function setupServer(server, wss, port) {
 }
 
 // 生存確認
+let intervalCnt = 0;
 setInterval(() => {
     const t1 = performance.now();
     // console.log("setInterval:" + t1);
-    Object.values(spaceAry).forEach(space => {
+    Object.keys(spaceAry).forEach(spaceName => {
+        const space = spaceAry[spaceName];
         const playerAry = space.playerAry;
         playerAry.forEach(client => {
             if (client != null) {
-                if (t1 - client.pingTime >= 30000) {
+                if (t1 - client.pingTime >= timeOutTime) {
+                    // タイムアウトで切断
                     const pos = playerAry.findIndex((client2) => client2 ? client2.ws == client.ws : false);
+                    console.log(
+                        "timeout",
+                        "playerNo", pos,
+                        "ip", client.ip,
+                        "port", client.port,
+                        "pathname", spaceName
+                    );
                     if (pos != -1) {
+                        playerAry[pos].isClose = true;
                         playerAry[pos] = null;
                         if (space.playerCnt > 0) space.playerCnt--;
+                        // 切断したことを全員に知らせる
+                        sendEvery(playerAry, JSON.stringify({
+                            mes: "_close",
+                            playerNo: pos,
+                            serverTime: new Date().getTime()
+                        }));
+                        // client.ws.close(); // Linuxだとoncloseが呼ばれない。ws 8.3.0のバグ？
                     }
-                    console.log("timeout", "playerCnt", pos);
-                } else {
-                    client.ws.send(JSON.stringify({ mes: "_ping" }));
                 }
             }
         });
     });
 
-    // 全部屋出力
+    // 全空間の情報出力
     if (false) { // debug log
-        Object.keys(spaceAry).forEach(key => {
-            const space = spaceAry[key];
-            // console.log("space:", key, "playerCnt:", space.playerCnt);
-            space.playerAry.forEach((client, clientI) => {
-                if (client != null) {
-                    console.log("space:", key, "playerNo:", clientI, "pingTime:", client == null ? "null" : t1 - client.pingTime, "ip:", client.ip, "port", client.port);
-                }
+        if (++intervalCnt % 5 == 0) {
+            console.log("--------", new Date(Date.now() - (new Date().getTimezoneOffset()) * 60 * 1000));
+            Object.keys(spaceAry).forEach(key => {
+                const space = spaceAry[key];
+                if (space.playerCnt >= 1) console.log("space:", key, "playerCnt:", space.playerCnt);
+                space.playerAry.forEach((client, clientI) => {
+                    if (client != null) {
+                        console.log("space:", key, "playerNo:", clientI, "pingTime:", client == null ? "null" : t1 - client.pingTime, "ip:", client.ip, "port", client.port);
+                    }
+                });
             });
-        });
+        }
     }
-}, 5000);
+}, 1000);
+
+
+function sendEvery(playerAry, sendStr) {
+    playerAry.forEach(client => {
+        if (client != null) client.ws.send(sendStr);
+    });
+}
 
